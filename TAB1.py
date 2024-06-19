c617af337e4c3165baffac6673de390da2a0777e
DRS4 <drsmm8904@gmail.com>
2024-06-19 20:05:28 +0200

    whas tired today but did some coding

--- TAB1.py
+++ TAB1.py
@@ -1,22 +1,143 @@
-class Solution(object):
-    def romanToInt(self, s):
-        sum = 0
-        roman_numerals = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000,
-}
-        priv = 0
-        for i in reversed(s):
-            cornv =roman_numerals[i]
-            if cornv >= priv:
-                sum+=cornv
-                priv = cornv
-            else:
-                sum-=cornv
-                privr = cornv
-        return sum
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+    
+    def prepend(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail=new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length +=1
+        return True
+    def prepop(self,value):
+        if self.length == 0:
+            return None
+        tmp = self.head
+        self.head = self.head.next
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return tmp
+    def get(self,index):
+        if index < 0 or index >= self.length:
+            return None
+        tmp = self.head
+        for _ in range(index):
+            tmp = tmp.next
+        return tmp
+    def set_value(self,index,value):
+        tmp = self.get(index)
+        if tmp:
+            tmp.value = value
+            return True
+        return False
+    def insert(self,index,value):
+        if index > self.length:
+            return False
+        if index == 0:
+            self.prepend(value)
+        if index == self.length:
+            self.append(value)
+        new_node = Node(value)
+        tmp = self.get(index-1)
+        new_node.next = tmp.next
+        tmp.next = new_node
+        self.length +=1
+        return True
+    def remove(self,index):
+        if index < self.length or index >= self.length:
+            return False
+        if index == 0:
+            self.prepop()
+        if index == self.length() -1:
+            self.pop()
+        pre = self.get(index-1)
+        tmp = pre.next
+        pre.next = tmp.next
+        tmp.next = None
+        self.length += 1
+        return tmp
+    def revers(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        befor = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = befor
+            befor = temp
+            temp = after
+            
+        
+        
+        
+        
+        
+        
+            
+
+my_linked_list = LinkedList(0)
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+
+# (2) Items - Returns 2 Node
+print(my_linked_list.get(3).value)
+# (1) Item -  Returns 1 Node
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    2
+    1
+    None
+
+"""
--- TAB2.py
+++ TAB2.py
@@ -1,30 +1,27 @@
-# class Node:
-    ## WRITE NODE CONSTRUCTOR HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
-
 class Node:
     def __init__(self,data):
         self.data = data
         self.next = None
         
-                
-# class LinkedList:
-    ## WRITE LL CONSTRUCTOR HERE ##
-    #                             #
-    #                             #
-    #                             #
-    #                             #
-    ###############################
 class LinkedList:
     def __init__(self,data):
         newNode = Node(data)
         self.head = newNode
         self.tail = newNode
         self.length = 1
+        def append(self,data):
+            newNode = Node(data)
+            if self.head == None:
+                self.head = data
+                self.tail = data
+                self.length = 1
+            else:
+                self.tail.next = newNode
+                self.tail = newNode
+            self.length += 1
+                
+            
+            
         
 
 
@@ -32,8 +29,8 @@ class LinkedList:
  
 my_linked_list = LinkedList(4)
 
-print('Head:', my_linked_list.head.value)
-print('Tail:', my_linked_list.tail.value)
+print('Head:', my_linked_list.head.data)
+print('Tail:', my_linked_list.tail.data)
 print('Length:', my_linked_list.length)
 
 

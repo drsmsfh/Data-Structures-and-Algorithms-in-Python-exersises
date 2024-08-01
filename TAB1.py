f1e0bcbc0b897cf7fc79e5311f0deaa6fc8a4ec1
DRS4 <drsmm8904@gmail.com>
2024-08-01 20:30:55 +0200

    I was tring to learn quic sort

--- TAB1.py
+++ TAB1.py
@@ -1,25 +1,14 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
-    def sortList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        tmp = head
-        listy = []
-        while tmp is not None:
-            listy.append(tmp.val)
-            tmp = tmp.next
-        def insertion_sort(my_list):
-            for i in range(1, len(my_list)):
-                temp = my_list[i]
-                j = i-1
-                while temp < my_list[j] and j > -1:
-                    my_list[j+1] = my_list[j] 
-                    my_list[j] = temp
-                    j -= 1
-            return my_list
+def swap(my_list,index1,index2):
+    tmp = my_list[index1]
+    my_list[index1] = my_list[index2]
+    my_list[index2] = tmp
+    
+def pivot(my_list, pivot_index, end_index):
+    swap_index = pivot_index
+    for i in range(pivot_index+1,end_index+1):
+        if my_list[swap_index] > my_list[i]:
+            swap(my_list,swap_index,i)
+            swap_index+=1
+        
+        swap(my_list,pivot_index,swap_index)
+        return swap_index
